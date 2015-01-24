9c28cb9de7e491a94bcbd04771b7d17133e22b2f
Petr Penzin <penzin.dev@gmail.com>
2015-01-23 20:03:38 -0800

    Enabled checking for portmaster and portupgrade

--- src/Config/FreeBSD/PortTools.hs
+++ src/Config/FreeBSD/PortTools.hs
@@ -1,18 +1,34 @@
 {-| FreeBSD tools to manage ports
  -}
-module Config.FreeBSD.PortTools where
+module Config.FreeBSD.PortTools (isPortmasterPresent, isPortupgradePresent) where
 
 import System.Process
 import System.Exit
+import Control.Exception
+
+{-| Run portmaster --help and check exit status
+ -}
+checkPortmaster :: IO Bool
+checkPortmaster = readProcessWithExitCode "portmaster" ["--help"] "" >>= \(status, _, _) -> return (status == ExitSuccess)
+
+{-| Run portupgrade --help and check exit status
+ -}
+checkPortupgrade :: IO Bool
+checkPortupgrade = readProcessWithExitCode "portupgrade" ["--help"] "" >>= \(status, _, _) -> return (status == ExitSuccess)
+
+{-| Return false for any exceptions
+ -}
+handler :: SomeException -> IO Bool
+handler _ = return False
 
 {-| Check whether portmaster is installed by simply looking if we can run the command
  -}
 isPortmasterPresent :: IO Bool
-isPortmasterPresent = readProcessWithExitCode "portmaster" ["--help"] "" >>= \(status, _, _) -> return (status == ExitSuccess)
+isPortmasterPresent = catch checkPortmaster handler
 
 {-| Check whether portupgrade is installed by simply looking if we can run the command
  -}
 isPortupgradePresent :: IO Bool
-isPortupgradePresent = readProcessWithExitCode "portupgrade" ["--help"] "" >>= \(status, _, _) -> return (status == ExitSuccess)
+isPortupgradePresent = catch checkPortupgrade handler
 
 
